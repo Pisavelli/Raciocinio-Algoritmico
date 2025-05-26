655a1ef4a157a2dc8503a2762704d831222cf5b0
Pierre <pierresavelli22@gmail.com>
2025-05-25 21:17:59 -0300

    Novos arquivos na pasta funções.

---
+++ funcoes/geometria.py
@@ -0,0 +1,10 @@
+import math
+
+def area_quadrado(lado):
+    return lado ** 2
+
+def area_triangulo(base, altura):
+    return base * altura / 2
+
+def area_circulo(raio):
+    return math.pi * raio ** 2
---
+++ funcoes/geometria_teste.py
@@ -0,0 +1,6 @@
+import geometria
+
+# Cálculo das áreas
+print(f"Área do quadrado (lado = 5): {geometria.area_quadrado(5)}")
+print(f"Área do triângulo (base = 10, altura = 6): {geometria.area_triangulo(10, 6)}")
+print(f"Área do círculo (raio = 3): {geometria.area_circulo(3)}")
