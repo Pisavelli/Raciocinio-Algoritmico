9530539900cba01d06c376779c270a29a70dc9cd
Pierre <pierresavelli22@gmail.com>
2025-05-25 21:29:10 -0300

    Fixed.

--- funcoes/geometria.py
+++ funcoes/geometria.py
@@ -1,10 +1,10 @@
 import math
 
-def area_quadrado(a):
-    return a**2
+def area_quadrado(lado):
+    return lado ** 2
 
-def area_triangulo(b, h):
-    return b*h / 2
+def area_triangulo(base, altura):
+    return base * altura / 2
 
-def area_circulo(r):
-    return math.pi * r**2
+def area_circulo(raio):
+    return math.pi * raio ** 2
