d7409c2d6b4ede8c0c7537577377812c12a39c60
Pierre <pierresavelli22@gmail.com>
2025-05-25 21:27:07 -0300

    Arrumando geometria.py.

--- funcoes/geometria.py
+++ funcoes/geometria.py
@@ -1,6 +1,4 @@
 import math
-<<<<<<< HEAD
-import cmath
 
 def area_quadrado(a):
     return a**2
@@ -10,14 +8,3 @@ def area_triangulo(b, h):
 
 def area_circulo(r):
     return math.pi * r**2
-=======
-
-def area_quadrado(lado):
-    return lado ** 2
-
-def area_triangulo(base, altura):
-    return base * altura / 2
-
-def area_circulo(raio):
-    return math.pi * raio ** 2
->>>>>>> teste
